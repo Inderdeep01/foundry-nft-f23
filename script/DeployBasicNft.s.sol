cf77f18f25746731dee10808ce1b23b88fb0393a
inderdeep <inderdeepsidhu01@gmail.com>
2024-08-13 22:38:55 +0530

    dynamic nft

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -1,11 +1,16 @@
-// SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "src/BasicNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployBasicNft is Script {
-    function run() external returns(BasicNft){
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 public deployerKey;
+
+    function run() external returns (BasicNft) {
         vm.startBroadcast();
         BasicNft basicNft = new BasicNft();
         vm.stopBroadcast();
